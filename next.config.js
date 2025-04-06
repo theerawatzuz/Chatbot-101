aaf943cf837a732c98367b566e3514f7dccaab1d
TTTTTTTT <TTTTTTTT@example.com>
2025-04-06 12:31:18 +0700

    add:stanalone

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ["172.20.10.9"], // เพิ่ม IP ที่ต้องการอนุญาต
+  output: "standalone",
 };
 
 module.exports = nextConfig;
